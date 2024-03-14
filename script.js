39dd110a2082ed05b5047a8580e3ab49ff9511bd
Ostertje <56339275+Ostertje@users.noreply.github.com>
2024-03-14 15:11:41 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function sendRequest() {
+    var inputText = document.getElementById("inputText").value;
+
+    // Send inputText to model and handle response
+    // Example: You can use Fetch API to send a POST request to your model's API endpoint
+    // Example: fetch('/api/predict', { method: 'POST', body: JSON.stringify({ inputText }) })
+    //            .then(response => response.json())
+    //            .then(data => document.getElementById("output").innerText = data.output);
+}
